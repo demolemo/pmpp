1c7908216e4ecfd5a35e1cae8271a7fa47faa6c8
lemo <demolemo@protonmail.com>
2026-01-03 15:19:34 +0700

    implement naive matmul and implement naive tiled matmul on square matrices

--- chapter05/tiling_matmul.cu
+++ chapter05/tiling_matmul.cu
@@ -1,11 +1,54 @@
 #include <iostream>
 
 
-// general case tiled matmul with tiles loaded in the shared memory
-// A /in N x M, B /in M x K, C /in N x K
-// for simplicity we assume that N, M, K are all divisible by TILE_SIZE
-__global__ tiledMatmul(
-        float* A, float *B, float *C,
-        int N, int, M, int K,
-        int TILE_SIZE
-        )
+// square case tiled matmul 
+// A /in N x N, B /in N x N, C /in N x N
+// for the simple case we assume that N % TILE_SIZE == 0
+
+// algorithm:
+// take a tile from the first matrix
+// take a tile from the second matrix
+// do the naive matmul
+// index corresponds to what?
+//
+// in the first implementation each thread is responsible for calculating one tile completely
+
+// each thread calculates one output element without tiling
+// the problem with this approch is too low utilization of the GPU. we are doing too little ops and too many reads
+__global__ void naiveMatmul(
+    float* A, float* B, float* C, int N
+    ){
+    const int row_idx = blockDim.x * blockIdx.x + threadIdx.x;
+    const int col_idx = blockDim.y * blockIdx.y + threadIdx.y;
+
+    float P_val = 0;
+    for (int i = 0; i < N; ++i) {
+        P_val += A[row_idx * N + i] * B[i * N + col_idx];
+    }
+    C[row_idx * N + col_idx] = P_val;
+}
+
+// each thread calculates the whole tile in the output matrix
+// this approach is a little bit better than the last because we decrease the number of memory reads using coalescing
+__global__ void naiveTiledMatmul(
+    float* A, float *B, float *C,
+    int N, int TILE_SIZE
+    ) {
+    // index of the tile that we calculate
+    const int row_idx = blockDim.x * blockIdx.x + threadIdx.x;
+    const int col_idx = blockDim.y * blockIdx.y + threadIdx.y;
+
+    for (int elem_row = 0; elem_row < TILE_SIZE; ++elem_row) {
+        for (int elem_col = 0; elem_col < TILE_SIZE; ++elem_col) {
+
+            float P_val = 0;
+            for (int elem_idx = 0; elem_idx < N; ++elem_idx) {
+                // get relative index of the element to the start of the grid
+                P_val += A[N * (row_idx * TILE_SIZE + elem_row) + elem_idx] * B[N * elem_idx + TILE_SIZE * col_idx + elem_col];
+            }
+            C[N * (row_idx * TILE_SIZE + elem_row) + col_idx * TILE_SIZE + elem_col] = P_val;
+        }
+    }
+}
+
+
